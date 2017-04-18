9d00225b1d035365446a3d09db03e5e2482ebebc
Mahesh Vangala <vangalamaheshh@gmail.com>
2017-04-18 17:37:47 -0400

    added generate_config script

---
+++ scripts/generate_config.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+#vim: syntax=python tabstop=2 expandtab
+
+__author__ = "Mahesh Vangala"
+__email__ = "<vangalamaheshh@gmail.com>"
+__date__ = "Apr, 18, 2017"
+
+"""
+  Takes in --data <path/to/fastqs> and generates config.yaml
+
+  
+"""
+
+import argparse
+import os
+import yaml
+import glob
+import re
+from collections import OrderedDict
+
+def parseArgs():
+  parser = argparse.ArgumentParser()
+  parser.add_argument('-f', '--fastq_folder', required = True, help = "Provide an absolute path to folder with FastQs.")
+  args = parser.parse_args()
+  return args
+
+def orderedLoad(stream, Loader = yaml.Loader, object_pairs_hook = OrderedDict):
+  class OrderedLoader(Loader):
+    pass
+
+  def construct_mapping(loader, node):
+    loader.flatten_mapping(node)
+    return object_pairs_hook(loader.construct_pairs(node))
+    
+  OrderedLoader.add_constructor(yaml.resolver.BaseResolver.DEFAULT_MAPPING_TAG, construct_mapping)
+  return yaml.load(stream, OrderedLoader)
+
+def orderedDump(data, stream=None, Dumper=yaml.Dumper, **kwds):
+  class OrderedDumper(Dumper):
+    pass
+    
+  def _dict_representer(dumper, data):
+    return dumper.represent_mapping(yaml.resolver.BaseResolver.DEFAULT_MAPPING_TAG, data.items())
+    
+  OrderedDumper.add_representer(OrderedDict, _dict_representer)
+  return yaml.dump(data, stream, OrderedDumper, **kwds)
+
+
+def getFastqInfo(absPath):
+  leftmates = glob.glob(absPath + "/*_R1*fast*", recursive = False)
+  info = dict()
+  info["isolates"] = OrderedDict()
+  reObj = re.compile("(\w+?)_")
+  for leftmate in leftmates:
+    sample = reObj.search(os.path.basename(leftmate)).group(1)
+    rightmate = leftmate.replace("_R1", "_R2")
+    if os.path.isfile(rightmate):
+      info["isolates"][sample] = [leftmate, rightmate]
+    else:
+      info["isolates"][sample] = [leftmate]
+
+  return info
+    
+
+if __name__ == "__main__":
+  args = parseArgs()
+  info = getFastqInfo(args.fastq_folder) 
+  print(orderedDump(info, default_flow_style = False))
